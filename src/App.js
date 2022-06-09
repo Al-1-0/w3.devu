606e4f01c981d5d61c31f82659cfddc50363381b
Allan McCafferty <allan247archivist@gmail.com>
2022-06-09 14:22:51 +0100

    Add Sign Up Template

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import Home from './components/pages/Home';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import WebServices from './components/pages/WebServices';
 import DigitalServices from './components/pages/DigitalServices';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
   <>
   <Router>
       <Navbar />
-      <Routes>
-      <Route path='/' element={<Home />} />        
-      <Route path='/web-services' element={<WebServices />} />
-      <Route path='/digital-services' element={<DigitalServices />} />
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />        
+          <Route path='/web-services' element={<WebServices />} />
+          <Route path='/digital-services' element={<DigitalServices />} />
+          <Route path='/sign-up' element={<SignUp />} />      
+        </Routes>
       </Router>
   </>
   );
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import '../../App.css';
+import Footer from '../Footer';
 
 export default function SignUp() {
     return <h1 className="sign-up">CONTACT</h1>;
-
+        <>
+            <Footer />
+        </>
 }
